8f27b5eb0a2dd02e9d8e8d15dc5574d2be90eab2
CodeMat <codemat.biz@gmail.com>
2024-03-13 17:23:23 +0100

    updated

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,31 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "IMUYA FINANCE PORTAL",
-  description: "This is the official IMUYA finance portal. Our online finance database.",
+  description:
+    "This is the official IMUYA finance portal. Our online finance database.",
+  openGraph: {
+    title: "IMUYA FINANCE PORTAL",
+    description:
+      "This is the official IMUYA finance portal. Our online finance database.",
+    author: ["Matthew Chukwu"],
+    developer: ["Matthew Chukwu"],
+    url: "https://imuya-finance.vercel.app",
+    siteName: "IMUYA Finance",
+    images: [
+      {
+        url: "https://res.cloudinary.com/mctony17/image/upload/v1652614881/imuya/image.png",
+        width: 600,
+        height: 600,
+      },
+      {
+        url: "https://res.cloudinary.com/mctony17/image/upload/v1652614881/imuya/image.png",
+        width: 800,
+        height: 800,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
